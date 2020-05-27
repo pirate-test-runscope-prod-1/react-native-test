53398116cf4f2005bbb578433dd2a70fd0b643ba
Sergey Dolin <v-sedoli@micorosoft.com>
2020-05-27 13:48:11 +0500

    add test project files

---
+++ ios/Pods/Target Support Files/Pods-ReactNativeTest622-ReactNativeTest622Tests/Pods-ReactNativeTest622-ReactNativeTest622Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReactNativeTest622_ReactNativeTest622Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ReactNativeTest622_ReactNativeTest622Tests
+@end
---
+++ ios/Pods/Target Support Files/Pods-ReactNativeTest622-tvOS/Pods-ReactNativeTest622-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReactNativeTest622_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_ReactNativeTest622_tvOS
+@end
